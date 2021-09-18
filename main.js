11bb54d09877bf2651a185fa6d44912c81db576b
Sarah <75193877+Sa-2010@users.noreply.github.com>
2021-09-18 17:05:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+lipsX = 0;
+lipsY = 0;
+function preload() {
+    lipstick_lips = loadImage('Lipstick.png');
+}
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelloaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelloaded() {
+    console.log("PoseNet Is Initialized");
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+    //fill(255, 0, 0);
+    //stroke(255, 0, 0);
+    //circle(lipsX, lipsY, 20);
+    image(lipstick_lips, lipsX, lipsY, 30, 30);
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        lipsX = results[0].pose.lips.X;
+        lipsY = results[0].pose.lips.Y;
+        console.log("lips X = " + results[0].pose.lips.X);
+        console.log("lips Y = " + results[0].pose.lips.Y);
+    }
+}
